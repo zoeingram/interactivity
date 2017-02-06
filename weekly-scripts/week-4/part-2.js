0c17c0aa6aca753ae8f0ee2981c0662fe29157d4
Zoe Ingram <zoe.a.ingram@gmail.com>
2017-02-06 11:14:54 -0800

    added overCircle function

--- weekly-scripts/week-4/part-2.js
+++ weekly-scripts/week-4/part-2.js
@@ -3,8 +3,40 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  stroke(255);
-  ellipse(50, 50, 50, 50);
+  background(255);
+
+  var size = 50;
+  var positionX = 50;
+  var positionY = 100;
+  colorMode("RGB");
+  stroke(255, 141, 256);
+  ellipse(positionX, positionY, size, size);
+  noFill();
+  if(overCircle(25, 75, 75, 125, "red")){
+  }
+  // rect(100, 100, 100, 100);
+  // if(overRect(50, 150, 50, 150)){
+  //   fill("blue");
+  // }
+  // ellipse(500, 500, 200, 200);
+  // ellipse(430, 229, 250, 250);
+  // triangle(30, 150, 58, 20, 86, 150);
+  // triangle(150, 75, 200, 400, 300, 75);
 
 }
+
+function overCircle(lowx, highx, lowy, highy, color){
+
+  if(mouseX >= lowx && mouseX <= highx && mouseY >= lowy && mouseY <= highy){
+    console.log("over circle");
+    fill(255, 141, 256);
+
+   }
+}
+
+// function overRect(lowx, highx, lowy, highy, fill) {
+//   if(mouseX >= lowx && mouseX <= highx && mouseY >= lowy && mouseY <= highy){
+//     console.log("over rectangle");
+//     fill("blue");
+//
+//   }}
