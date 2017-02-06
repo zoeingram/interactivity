ec35a8fea82b2cdbc98934be88392b47cdc8fe04
Zoe Ingram <zoe.a.ingram@gmail.com>
2017-02-06 13:03:27 -0800

    added better code

--- weekly-scripts/week-4/part-2.js
+++ weekly-scripts/week-4/part-2.js
@@ -1,42 +1,42 @@
+var c1x = 100;
+var c1y = 200;
+var c1r = 200;
+
+var c2x = 300;
+var c2y = 100;
+var c2r = 80;
+
 function setup() {
-  createCanvas(600, 600);
+    createCanvas(600, 600);
 }
 
 function draw() {
-  background(255);
+    background(255);
 
-  var size = 50;
-  var positionX = 50;
-  var positionY = 100;
-  colorMode("RGB");
-  stroke(255, 141, 256);
-  ellipse(positionX, positionY, size, size);
-  noFill();
-  if(overCircle(25, 75, 75, 125, "red")){
-  }
-  // rect(100, 100, 100, 100);
-  // if(overRect(50, 150, 50, 150)){
-  //   fill("blue");
-  // }
-  // ellipse(500, 500, 200, 200);
-  // ellipse(430, 229, 250, 250);
-  // triangle(30, 150, 58, 20, 86, 150);
-  // triangle(150, 75, 200, 400, 300, 75);
+    if (overCircle(c1x, c1y, c1r)) {
+        fill(0, 102, 153);
+        noStroke();
+    } else {
+        noFill();
+        stroke(0, 102, 153);
+    }
+    ellipse(c1x, c1y, c1r * 2, c1r * 2);
+
+    if (overCircle(c2x, c2y, c2r)) {
+        fill(204, 102, 0);
+        noStroke();
+    } else {
+        noFill();
+        stroke(204, 102, 0);
+    }
+    ellipse(c2x, c2y, c2r * 2, c2r * 2);
 
 }
 
-function overCircle(lowx, highx, lowy, highy, color){
-
-  if(mouseX >= lowx && mouseX <= highx && mouseY >= lowy && mouseY <= highy){
-    console.log("over circle");
-    fill(255, 141, 256);
-
-   }
+function overCircle(x, y, radius) {
+    if (dist(x, y, mouseX, mouseY) < radius) {
+        return true;
+    } else {
+        return false;
+    }
 }
-
-// function overRect(lowx, highx, lowy, highy, fill) {
-//   if(mouseX >= lowx && mouseX <= highx && mouseY >= lowy && mouseY <= highy){
-//     console.log("over rectangle");
-//     fill("blue");
-//
-//   }}
